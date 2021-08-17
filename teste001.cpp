873f170a9c6d8751e289893851e5a626d36a9d8c
well204 <86674075+well204@users.noreply.github.com>
2021-08-17 09:32:40 -0300

    Update teste001.cpp

--- teste001.cpp
+++ teste001.cpp
@@ -20,10 +20,10 @@ typedef struct Carro{
 	
 motorista *vet = (motorista*) malloc(sizeof(motorista)); //vetor global de motoristas
 
-void add_driver(){ // funÁ„o para adicionarr motorista
+void add_driver(){ // fun√ß√£o para adicionarr motorista
    // criando vetor para cadastrar motoristas usando ele
   int i = 0, op = 1; 
-  while(op != 0){ // criando uma repetiÁ„o onde sempre que o usu·rio digite 1, seja possÌvel est· adicionando outro motorista
+  while(op != 0){ // criando uma repeti√ß√£o onde sempre que o usu√°rio digite 1, seja poss√≠vel est√° adicionando outro motorista
     printf("Digite um id para o motorista: ");
     scanf("%d",&(vet+i)->driver_id);
     
@@ -36,12 +36,12 @@ void add_driver(){ // funÁ„o para adicionarr motorista
     printf("\nInsira o nome do motorista: ");
     scanf("%s",(vet+i)->nome);
     
-    printf("\nDeseja adicionar mais um motorista?\n1-Sim\n0-N„o\n");
+    printf("\nDeseja adicionar mais um motorista?\n1-Sim\n0-N√£o\n");
     
     scanf("%d",&op);
     if(op != 0){ 
       i++;
-      vet = (motorista*) realloc(vet, (i+1)*sizeof(motorista)); // adicionando mais uma posiÁ„o ao vetor caso a resposto da usu·rio seja 1 
+      vet = (motorista*) realloc(vet, (i+1)*sizeof(motorista)); // adicionando mais uma posi√ß√£o ao vetor caso a resposto da usu√°rio seja 1 
     }
     
   }
@@ -50,7 +50,7 @@ void add_driver(){ // funÁ„o para adicionarr motorista
 
 carro *vet_car = (carro*) malloc(sizeof(carro)); // vetor global de carros
 	int i = 0;
-void add_car(){ // funÁ„o para adicionar carros
+void add_car(){ // fun√ß√£o para adicionar carros
 	int  op = 1;
 	while(op != 0){  
 		printf("Digite um id para o carro ");
@@ -62,46 +62,46 @@ void add_car(){ // funÁ„o para adicionar carros
 		printf("\nInsira o modelo do carro: ");
 		scanf("%s",(vet_car+i)->modelo);
 		
-		printf("\nDeseja adicionar mais um carro?\n1-Sim\n0-N„o\n");
+		printf("\nDeseja adicionar mais um carro?\n1-Sim\n0-N√£o\n");
 		scanf("%d",&op);
 		if(op != 0){
 			i++;
-			vet_car = (carro*) realloc(vet, (i+1)*sizeof(carro)); // asicionando mais uma posiÁ„o ao vetor caso a resposta do usu·rio sejaa 1
+			vet_car = (carro*) realloc(vet, (i+1)*sizeof(carro)); // asicionando mais uma posi√ß√£o ao vetor caso a resposta do usu√°rio sejaa 1
 		}
 	
    }
 
 }
 
-void remove_driver(){ // funÁ„o para remover motorisra
+void remove_driver(){ // fun√ß√£o para remover motorisra
 	int id;
-	printf("Digite o id do motorista que vocÍ deseja remover: ");
-	scanf("%d",id);
+	printf("Digite o id do motorista que voc√™ deseja remover: ");
+	scanf("%d",&id);
 	
 	for(int j =0; j> i; j++){
 	
 	if(id == vet->driver_id){
-		vet = (motorista*) realloc(vet, (-1)*sizeof(motorista)); // removendo a posiÁ„o do vetor
-	}else printf("Insira um id v·lido");
+		vet = (motorista*) realloc(vet, (-1)*sizeof(motorista)); // removendo a posi√ß√£o do vetor
+	}else printf("Insira um id v√°lido");
   }
 	
 }
 
-void remove_car(){ // funÁ„o para remover carro
+void remove_car(){ // fun√ß√£o para remover carro
 	int id;
-	printf("Digite o id do carro que vocÍ deseja remover: ");
-	scanf("%d",id);
+	printf("Digite o id do carro que voc√™ deseja remover: ");
+	scanf("%d",&id);
 	for(int j =0; j> i; j++){
 	if(id == (vet_car+i)->car_id){
-		vet = (motorista*) realloc(vet, (-1)*sizeof(motorista)); // removendo a posiÁ„o do vetor
-	}else printf("Insira um id v·lido");
+		vet = (motorista*) realloc(vet, (-1)*sizeof(motorista)); // removendo a posi√ß√£o do vetor
+	}else printf("Insira um id v√°lido");
 }
 }
 int main(){
 	
 	int cadastrar;
 	do{
-	printf("Escolha uma opÁ„o:\n1-Cadastrar motorista\n2-Cadastrar carro\n3-Remover motorista\n4-Remover carro\n5-Sair\n");
+	printf("Escolha uma op√ß√£o:\n1-Cadastrar motorista\n2-Cadastrar carro\n3-Remover motorista\n4-Remover carro\n5-Sair\n");
 	scanf("%d",&cadastrar);
 	
 	switch(cadastrar){
@@ -120,9 +120,9 @@ int main(){
 		case 4:
 			remove_car();
 	}
- }while(cadastrar <= 4 && cadastrar !=0); // laÁo 
+ }while(cadastrar <= 4 && cadastrar !=0); // la√ßo 
  
-	free(vet); // liberando memÛria 
-	free(vet_car);	// liberando memÛria
+	free(vet); // liberando mem√≥ria 
+	free(vet_car);	// liberando mem√≥ria
 	return 0;
 }
